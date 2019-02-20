1935056e24ec6f42e7ada8a3b2414b7e3b261809
Toufik JADLI <toufik.jadli@gmail.com>
2019-02-20 23:03:37 +0100

    Eject app

--- src/containers/header/index.js
+++ src/containers/header/index.js
@@ -7,7 +7,7 @@ export default class Header extends Component {
       <header>
         <nav className="navbar navbar-light bg-light">
           <a className="navbar-brand">
-            <img className="logo" src="/assets/images/logo.png" />
+            <img className="logo" alt="Logo" height="70px" src="/assets/images/logo.png" />
           </a>
 
         </nav>
